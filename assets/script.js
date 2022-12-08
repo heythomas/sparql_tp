c9ee52da99828b8c0119931f44a0c5dbb546a87f
Thomas <thomas0bellon@gmail.com>
2022-12-08 10:55:42 +0100

    graph implemented + css

--- assets/script.js
+++ assets/script.js
@@ -38,8 +38,88 @@ document.getElementById("war").addEventListener("change", function () {
         data.results.bindings.forEach(element => {
             L.marker([element.lat.value, element.long.value]).addTo(map).bindPopup('<a href="'+element.item.value+'">'+element.itemLabel.value+'</a>');
         });
+        d3sparql.forcegraph(data);
+        $('.forcegraph').hide();
     });
 })
+
+$('#toggler').click(function () {
+    $('.forcegraph').fadeToggle();
+});
+
+// Graph function
+
+d3sparql.forcegraph = function(json, config) {
+    config = config || {}
+  
+    var graph = (json.head && json.results) ? d3sparql.graph(json, config) : json
+  
+    var scale = d3.scale.linear()
+      .domain(d3.extent(graph.nodes, function(d) { return parseFloat(d.value) }))
+      .range([1, 20])
+  
+    var opts = {
+      "radius":    config.radius    || function(d) { return d.value ? scale(d.value) : 1 + d.label.length },
+      "charge":    config.charge    || -500,
+      "distance":  config.distance  || 50,
+      "width":     config.width     || 1000,
+      "height":    config.height    || 500,
+      "label":     config.label     || false,
+      "selector":  config.selector  || null
+    }
+  
+    var svg = d3sparql.select(opts.selector, "forcegraph").append("svg")
+      .attr("width", opts.width)
+      .attr("height", opts.height)
+    var link = svg.selectAll(".link")
+      .data(graph.links)
+      .enter()
+      .append("line")
+      .attr("class", "link")
+    var node = svg.selectAll(".node")
+      .data(graph.nodes)
+      .enter()
+      .append("g")
+    var circle = node.append("circle")
+      .attr("class", "node")
+      .attr("r", opts.radius)
+    var text = node.append("text")
+      .text(function(d) { return d[opts.label || "label"] })
+      .attr("class", "node")
+    var force = d3.layout.force()
+      .charge(opts.charge)
+      .linkDistance(opts.distance)
+      .size([opts.width, opts.height])
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .start()
+    force.on("tick", function() {
+      link.attr("x1", function(d) { return d.source.x })
+          .attr("y1", function(d) { return d.source.y })
+          .attr("x2", function(d) { return d.target.x })
+          .attr("y2", function(d) { return d.target.y })
+      text.attr("x", function(d) { return d.x })
+          .attr("y", function(d) { return d.y })
+      circle.attr("cx", function(d) { return d.x })
+            .attr("cy", function(d) { return d.y })
+    })
+    node.call(force.drag)
+  
+    // default CSS/SVG
+    link.attr({
+      "stroke": "#999999",
+    })
+    circle.attr({
+      "stroke": "black",
+      "stroke-width": "1px",
+      "fill": "lightblue",
+      "opacity": 1,
+    })
+    text.attr({
+      "font-size": "8px",
+      "font-family": "sans-serif",
+    })
+  }
 
 // Init
 var endpointUrl = 'https://query.wikidata.org/sparql',
@@ -74,4 +154,6 @@ makeSPARQLQuery( endpointUrl, sparqlQuery, function( data ) {
     data.results.bindings.forEach(element => {
         L.marker([element.lat.value, element.long.value]).addTo(map).bindPopup('<a href="'+element.item.value+'">'+element.itemLabel.value+'</a>');
     });
+    d3sparql.forcegraph(data)
+    $('.forcegraph').hide();
 });
